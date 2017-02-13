6253a9cc38f65f4518f78b897d6973ea929eb377
Leo Moon <leomoondev@gmail.com>
2017-02-13 11:39:13 -0800

    Added 220 width and 100 height for three image views

--- PoliceSketch/LPSViewController.m
+++ PoliceSketch/LPSViewController.m
@@ -10,6 +10,16 @@
 
 @interface LPSViewController ()
 
+@property (strong, nonatomic) IBOutlet UIButton *firstLeftArrowButton;
+@property (strong, nonatomic) IBOutlet UIButton *firstRightArrowButton;
+@property (strong, nonatomic) IBOutlet UIButton *secondLeftArrowButton;
+@property (strong, nonatomic) IBOutlet UIButton *secondRightArrowButton;
+@property (strong, nonatomic) IBOutlet UIButton *thirdLeftArrowButton;
+@property (strong, nonatomic) IBOutlet UIButton *thirdRightArrowButton;
+@property (strong, nonatomic) IBOutlet UIImageView *eyesView;
+@property (strong, nonatomic) IBOutlet UIImageView *noseView;
+@property (strong, nonatomic) IBOutlet UIImageView *mouthView;
+
 @end
 
 @implementation LPSViewController
@@ -19,6 +29,14 @@
     [super viewDidLoad];
 	
     // Here is where you will create the buttons & image views and add them to the view.
+    
+//    UIImage *eyesImage = [UIImage imageNamed:@"great_pic.png"];
+//
+//    self.eyesView.image = eyesImage;
+//    
+//    [self.eyesView setFrame:CGRectMake(76.42, 56, 220, 100)];
+//    
+
 }
 
 @end
